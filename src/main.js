af5a9aabc643cc355e84b3c4924fe3b7d208da4a
sh <sh770h@gmail.com>
2022-12-20 00:17:01 +0200

    vanila logout

--- src/main.js
+++ src/main.js
@@ -3,16 +3,20 @@
 function login() {
     // window.location.assign("/login/src/login.html")
     window.location.assign("/login/src/login.html")
-  }
+}
 
-function checklog(){
-    if (localStorage.getItem("user", "login") )
-       {
+function checklog() {
+    if (localStorage.getItem("user", "login")) {
         console.log("login is true")
-    }else{
+    } else {
         console.log("login is false")
-    window.location.assign("/VanillaProject/login/src/login.html")
+        window.location.assign("/VanillaProject/login/src/login.html")
     }
 }
 
 checklog();
+
+function logout() {
+    localStorage.clear();
+    window.location.assign("/VanillaProject/login/src/login.html")
+}
